52826e04f088e6a53499d20ac9cada7d5bc47790
imperialhealthdev <57159778+imperialhealthdev@users.noreply.github.com>
2019-12-05 15:04:13 -0800

    Add files via upload

--- popover.js
+++ popover.js
@@ -1,6 +1,16 @@
 $('#filter-button')
   .popup({
     popup : $('.custom.popup'),
-    on    : 'click'
+    on    : 'click',
+    lastResort: 'bottom left',
+        onShow: function(){
+            resizePopup();
+    },
   })
 ;
+
+var resizePopup = function(){$('.ui.popup').css('max-height', $(window).height());};
+
+$(window).resize(function(e){
+    resizePopup();
+});
