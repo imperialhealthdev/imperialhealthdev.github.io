dda8869458da44c22a439361fe53ebd32235d335
imperialhealthdev <57159778+imperialhealthdev@users.noreply.github.com>
2021-04-12 10:41:29 -0700

    updated site plus ui filters change

--- controller.js
+++ controller.js
@@ -1,74 +1,79 @@
 var app = angular.module('myApp', []);
-app.controller('providersCtrl', function($scope, $http) {
-$scope.quantity = 12;
-
-  $http.get("https://imperialhealthdev.github.io/providers.json").then(function(response) {
-      $scope.myData = response.data.data;
-  });
+app.controller('providersCtrl', function ($scope, $http) {
+    $scope.quantity = 12;
+    $scope.loading = true;
+    $http.get("https://imperialhealthdev.github.io/providers.json").then(function (response) {
+        
+        $scope.myData = response.data.data;
+        $scope.loading = false;
+        
+        
+        var cities = [];
+        $.each(response.data.data, function(index, value) {
+        if ($.inArray(value.City, cities)==-1) {
+        cities.push(value.City);
+        }
+        });
+        $scope.filteredCity = cities;
+        
+        var states = [];
+        $.each(response.data.data, function(index, value) {
+        if ($.inArray(value.State, states)==-1) {
+        states.push(value.State);
+        }
+        });
+        $scope.filteredState = states;
+        
+        
+        var languages = [];
+        $.each(response.data.data, function(index, value) {
+        if ($.inArray(value.Language, languages)==-1) {
+        languages.push(value.Language);
+        }
+        });
+        $scope.filteredLang = languages;
+        
+        
+        var specialties = [];
+        $.each(response.data.data, function(index, value) {
+        if ($.inArray(value.Specialty, specialties)==-1) {
+        specialties.push(value.Specialty);
+        }
+        });
+        $scope.filteredSpecialty = specialties;
+       
     
-      $scope.clearFilter = function() {
-      $scope.query = "";
-      $scope.cc = {};
-    };
-    
-    
-  $scope.loadMore = function() {
-      var incremented = $scope.quantity + 36;
-      $scope.quantity = incremented > $scope.myData.length ? $scope.myData.length : incremented;
-    };
-   
 });
+        
 
 
-//filter to remove duplicates for dropdown buttons and show unique values
-app.filter('unique', function () {
+    $scope.clearFilter = function () {
+        $scope.query = "";
+        $scope.cityFilter = "";
+        $scope.stateFilter = "";
+        $scope.langFilter = "";
+        $scope.specFilter = "";
+    };
 
-        return function (items, filterOn) {
 
-            
-            if (filterOn === false) {
-                return items;
-            }
+    $scope.loadMore = function () {
+        var incremented = $scope.quantity + 36;
+        $scope.quantity = incremented > $scope.myData.length ? $scope.myData.length : incremented;
+    };
+    
 
-            if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-                var hashCheck = {}, newItems = [];
+//unique filter
 
-                var extractValueToCompare = function (item) {
-                    if (angular.isObject(item) && angular.isString(filterOn)) {
-                        return item[filterOn];
-                    } else {
-                        return item;
-                    }
-                };
-
-                angular.forEach(items, function (item) {
-                    var valueToCheck, isDuplicate = false;
-
-                    for (var i = 0; i < newItems.length; i++) {
-                        if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-                            isDuplicate = true;
-                            break;
-                        }
-                    }
-                    if (!isDuplicate) {
-                        newItems.push(item);
-                    }
-
-                });
-                items = newItems;
-            }
-            return items;
-        };
     });
 
-app.directive("directiveWhenScrolled", function() {
-  return function(scope, elm, attr) {
-    var raw = elm[0];
+app.directive("directiveWhenScrolled", function () {
+    return function (scope, elm, attr) {
+        var raw = elm[0];
 
-    elm.bind('scroll', function() {
-      if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-        scope.$apply(attr.directiveWhenScrolled);
-      }
-    });
-  };
+        elm.bind('scroll', function () {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.directiveWhenScrolled);
+            }
+        });
+    };
 });
